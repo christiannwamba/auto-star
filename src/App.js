d66856c63f3f74b73a71b57e5b8e118f7b5cf2e3
Christian Nwamba <nwambachristian@gmail.com>
2019-04-16 08:20:57 +0100

    update url

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import './App.css';
 
 class App extends Component {
   url =
-    process.env.NODE_ENV !== 'production'
+    process.env.NODE_ENV === 'production'
       ? 'https://autostar17.azurewebsites.net/api/'
       : 'http://localhost:7071/api/';
 
